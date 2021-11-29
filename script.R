f1a42ededee9113989689241c36c104637116d68
robbindewaal <rs.dewaal@tilburguniversity.edu>
2021-11-29 12:55:31 +0100

    Finished analyses

--- script.R
+++ script.R
@@ -2,38 +2,27 @@
 library(readxl)
 library(readxl)
 library(car)
-AllFields_with_group_membership <- read_excel("data/AllFields with group membership.xlsx")
+library(tidyverse)
+AllFields_segments<- read_excel("data/AllFields with group membership.xlsx")
+View(AllFields_segments)
 
-# ANOVA
-gender_aov <- lm(Gender ~ AllFields_with_group_membership$`4 Group Membership`, data = AllFields_with_group_membership)
-summary(gender_aov)
 
+# Chi-square test - For nominal or ordinal variables
+chisq.test(AllFields_segments$Gender)
+chisq.test(AllFields_segments$Education)
+chisq.test(AllFields_segments$Occupation)
+chisq.test(AllFields_segments$LinearTV)
+
+
+# ANOVA - For ratio and interval variables
 age_aov <- lm(Age ~ AllFields_with_group_membership$`4 Group Membership`, data = AllFields_with_group_membership)
 summary(age_aov)
 
-education_aov <- lm(Education ~ AllFields_with_group_membership$`4 Group Membership`, data = AllFields_with_group_membership)
-summary(education_aov)
-
-occupation_aov <- lm(Occupation ~ AllFields_with_group_membership$`4 Group Membership`, data = AllFields_with_group_membership)
-summary(occupation_aov)
-income_aov <- lm(Income ~ AllFields_with_group_membership$`4 Group Membership`, data = AllFields_with_group_membership)
-summary(income_aov)
-
 streamingservices_aov <- lm(AllFields_with_group_membership$StreamingServices ~ AllFields_with_group_membership$`4 Group Membership`, data = AllFields_with_group_membership)
 summary(streamingservices_aov)
-
-lineartv_aov <- lm(LinearTV ~ AllFields_with_group_membership$`4 Group Membership`, data = AllFields_with_group_membership)
-summary(lineartv_aov)
 
 tvknowledge_aov <- lm(TVknowledge_r1 ~ AllFields_with_group_membership$`4 Group Membership`, data = AllFields_with_group_membership)
 summary(tvknowledge_aov)
 
 
 
-
-# Chi-square test
-chisq.test(AllFields_with_group_membership$Gender)
-chisq.test(AllFields_with_group_membership$Education)
-chisq.test(AllFields_with_group_membership$Age)
-chisq.test(AllFields_with_group_membership$Occupation)
-chisq.test(AllFields_with_group_membership$Income)
